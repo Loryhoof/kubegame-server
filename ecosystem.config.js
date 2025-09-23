74fdb0582b726c3ee1c2d5b23aaf44265e65464d
Kevin Klatt <klattkev@gmail.com>
2025-09-23 23:35:58 +0700

    adsdas

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "myapp",
+      script: "node_modules/.bin/ts-node-dev",
+      args: "--respawn src/index.ts",
+      watch: false, // you can set true if you want auto-reload
+      env: {
+        PORT: 443,
+        ENVIRONMENT: "PROD",
+      },
+    },
+  ],
+};
