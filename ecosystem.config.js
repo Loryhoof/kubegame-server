aea79ab51a54e6b14bbafbd824ae8b2156c87583
Kevin Klatt <klattkev@gmail.com>
2025-09-30 00:14:07 +0200

    change to server.ts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "myapp",
       script: "node_modules/.bin/ts-node-dev",
-      args: "--respawn src/index.ts",
+      args: "--respawn src/server.ts",
       watch: false, // you can set true if you want auto-reload
       env: {
         PORT: 443,
